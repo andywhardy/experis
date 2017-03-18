677c072665559e6007b64cd2b609778598d6e42d
andywhardy <andywhardy@gmail.com>
2017-03-18 21:28:47 +0000

    Experis: further implementation and refinement of bill

--- src/main/scala/Bill.scala
+++ src/main/scala/Bill.scala
@@ -1,4 +1,26 @@
-class Bill {
-  def calculateTotalPrice(menuItems: List[String]): String = "3.5"
+import Menu.items
+
+object Bill {
+
+  def calculateTotalPrice(menuItems: List[String]): String = {
+    if(menuItems.isEmpty) {
+      "£0.00"
+    } else {
+      val menuItemList = createMenuItemList(menuItems)
+      val totalPrice = totalPriceForMenuItems(menuItemList)
+
+      poundsAndPence(totalPrice)
+    }
+  }
+
+  def totalPriceForMenuItems(menuItemList: List[MenuItem]): Int = {
+    menuItemList.foldLeft(0)((acc, menuItem: MenuItem) => menuItem.price + acc)
+  }
+
+  def createMenuItemList(menuItems: List[String]): List[MenuItem] = {
+    menuItems.map(menuItem => items()(menuItem))
+  }
+
+  def poundsAndPence(pence: Int): String = s"£${pence / 100}.${pence % 100}"
 
 }
--- src/test/scala/BillSpec.scala
+++ src/test/scala/BillSpec.scala
@@ -1,12 +1,23 @@
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
 class BillSpec extends FlatSpec with Matchers {
 
-  "A bill of cola, coffee and a cheese sandwich" should "be 3.5" in {
-    val bill = new Bill()
-    val totalPrice = bill.calculateTotalPrice(List("Cola", "Coffee", "Cheese Sandwich"))
+  "An empty bill" should "be £0.00" in {
+    val totalPrice = Bill.calculateTotalPrice(List())
 
-    totalPrice should be ("3.5")
+    totalPrice should be ("£0.00")
+  }
+
+  "A bill of cola, coffee and a cheese sandwich" should "be £3.50" in {
+    val totalPrice = Bill.calculateTotalPrice(List("Cola", "Coffee", "Cheese Sandwich"))
+
+    totalPrice should be ("£3.50")
+  }
+
+  "A bill of coffee and a steak sandwich" should "be 5.50" in {
+    val totalPrice = Bill.calculateTotalPrice(List("Coffee", "Steak Sandwich"))
+
+    totalPrice should be ("£5.50")
   }
 
 }
